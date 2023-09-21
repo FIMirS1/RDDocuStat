35d00b67708b7fe0e73608d39d8777847b2779ab
Mirco Thissen <97433828+FIMirS1@users.noreply.github.com>
2023-09-21 14:42:51 +0200

    .

---
+++ Projekt_LF12/connector.php
@@ -0,0 +1,33 @@
+<?php
+// Datenbankverbindung herstellen
+$servername = "localhost"; // Ihr Datenbank-Host
+$username = "root"; // Ihr Datenbank-Benutzername
+$password = "root"; // Ihr Datenbank-Passwort
+$dbname = "rd_stat"; // Ihr Datenbank-Name
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verbindung überprüfen
+if ($conn->connect_error) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+}
+
+// Benutzereingaben aus dem Formular abrufen
+$patientname = $_POST["patientname"];
+$patientvorname = $_POST["patientvorname"];
+$patientbday = $_POST["patientbday"];
+$einsatznr = $_POST["einsatznr"]
+
+
+// SQL-Abfrage erstellen, um Daten einzufügen
+$sql = "INSERT INTO IhreTabelle (Vorname, Nachname, Email) VALUES ('$vorname', '$nachname', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Daten erfolgreich in die Datenbank eingefügt.";
+} else {
+    echo "Fehler beim Einfügen der Daten: " . $conn->error;
+}
+
+// Datenbankverbindung schließen
+$conn->close();
+?>
